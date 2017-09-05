3c02d1c36d11f6d707ad3a8e3078873f04ef1145
Paul Doyle <pauldoyle22@gmail.com>
2017-09-05 01:42:32 -0700

    Beauter wins because it's sooooo much less boilerplate

--- main.py
+++ main.py
@@ -2,15 +2,27 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 template_folder = '../templates'
+app.debug = True
+
+def render_active_page(template, name=''):
+    return render_template(template, active=name)
 
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+    return render_active_page('index.html')
 
 @app.route('/resume')
 def resume_page():
-    return render_template('resume.html')
+    return render_active_page('resume.html', 'resume')
 
-@app.rout('/about')
+@app.route('/about')
 def about_page():
-    return render_template('about.html')
+    return render_active_page('about.html', 'about')
+
+@app.route('/contact')
+def contact_page():
+    return render_active_page('contact.html', 'contact')
+
+@app.route('/projects')
+def projects_page():
+    return render_active_page('projects.html', 'projects')
