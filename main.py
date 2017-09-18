0aca6cb27bf1e00e4072f0c4be526bdef94a675f
Paul Doyle <pauldoyle22@gmail.com>
2017-09-17 17:38:10 -0700

    More resume adjustments.  Working towards a complete draft of the website.

--- main.py
+++ main.py
@@ -14,15 +14,3 @@ def main_page():
 @app.route('/resume')
 def resume_page():
     return render_active_page('resume.html', 'resume')
-
-@app.route('/about')
-def about_page():
-    return render_active_page('about.html', 'about')
-
-@app.route('/contact')
-def contact_page():
-    return render_active_page('contact.html', 'contact')
-
-@app.route('/projects')
-def projects_page():
-    return render_active_page('projects.html', 'projects')
